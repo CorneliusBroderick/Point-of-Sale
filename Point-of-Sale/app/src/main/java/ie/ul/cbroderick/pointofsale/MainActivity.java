5a539120bff51e904bccbd7fcfae23deb064748c
CorneliusBroderick <9119124@studentmail.ul.ie>
2018-10-11 11:12:39 +0100

    Snackbar Video 4 done

--- Point-of-Sale/app/src/main/java/ie/ul/cbroderick/pointofsale/MainActivity.java
+++ Point-of-Sale/app/src/main/java/ie/ul/cbroderick/pointofsale/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mNameTextView, mQuantityTextView, mDateTextView;
     private Item mCurrentItem;
+    private Item mClearedItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,8 +70,21 @@ public class MainActivity extends AppCompatActivity {
 
         switch (item.getItemId()){
             case R.id.action_reset:
+                mClearedItem = mCurrentItem;
                 mCurrentItem = new Item();
                 showCurrentItem();
+                Snackbar snackbar = Snackbar.make(findViewById(R.id.coordinator_layout),
+                        "Item cleared", Snackbar.LENGTH_LONG);
+                snackbar.setAction("UNDO", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mCurrentItem = mClearedItem;
+                        showCurrentItem();
+                        Snackbar.make(findViewById(R.id.coordinator_layout),
+                                "Item restored", Snackbar.LENGTH_SHORT).show();
+                    }
+                });
+                snackbar.show();
                 return true;
             case R.id.action_settings:
                 //startActivity(new Intent((Settings.ACTION_SETTINGS)));
